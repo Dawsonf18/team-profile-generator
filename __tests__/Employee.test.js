f35c945c4ca94c182cdf22936b03e54b9b8401c7
Dawson Filion <Dawsonfilion21@gmail.com>
2022-04-25 16:51:31 -0400

    finished project

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,27 +3,27 @@ const Employee = require('../lib/Employee')
 describe('Employee Class', () => {
     describe('Initialization', () => {
         test('Should create an object with a name', () => {
-            const employee = new Employee('Tim', 123, 'Tim@gmail.com');
+            const employee = new Employee('Tim', 123, 'tim@gmail.com');
 
             expect(employee.getName()).toEqual('Tim');
         });
 
         test('Should create an object with an id', () => {
-            const employee = new Employee('Tim', 123, 'Tim@gmail.com');
+            const employee = new Employee('Tim', 123, 'tim@gmail.com');
 
             expect(employee.getId()).toEqual(123);
           
         });
 
         test('Should create an object with an email', () => {
-            const employee = new Employee('Tim', 123, 'Tim@gmail.com');
+            const employee = new Employee('Tim', 123, 'tim@gmail.com');
 
             expect(employee.getEmail()).toEqual('Tim@gmail.com');
           
         });
 
-        test('Should create an object with an id', () => {
-            const employee = new Employee('Tim', 123, 'Tim@gmail.com');
+        test('Should create an object with a role', () => {
+            const employee = new Employee('Tim', 123, 'tim@gmail.com');
 
             expect(employee.getRole()).toEqual('Employee');
           
